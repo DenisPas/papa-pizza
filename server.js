2b4aeca1c885141f3604964063aed48fd64ec28c
denis <pas@happy-inc.ru>
2022-02-07 10:44:51 +0300

    fix

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json'); // <== Will be created later
-const middlewares = jsonServer.defaults();
-const port = process.env.REACT_APP_PORT || 3001; // <== You can change the port
-
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const PORT = process.env.REACT_APP_PORT;
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/pizzas/*': '/$1',
+}))
 server.use(router);
-
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log('Server is running');
 });
